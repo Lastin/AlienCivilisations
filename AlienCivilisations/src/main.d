4b98e224f4787dbaf1e88d132b56d1a5a506807b
Lastin <maksymsmail@gmail.com>
2016-01-05 23:35:49 +0000

    played around with layouts

--- AlienCivilisations/src/main.d
+++ AlienCivilisations/src/main.d
@@ -1,13 +1,32 @@
 ﻿module src.main;
 
-import std.stdio;
-import src.handlers.gameManager;
-import src.entities.map;
-import src.containers.vector2d;
-import src.entities.planet;
+import dlangui;
 import src.states.menu;
 import src.states.play;
+import src.states.state;
 
-void main(string[] args) {
+mixin APP_ENTRY_POINT;
 
+extern (C) int UIAppMain(string[] args){
+	Window window = Platform.instance.createWindow("Alien Civilisations", null, WindowFlag.Resizable, 1024, 768);
+	auto btn = new Button();
+	// change some properties
+	btn.text = "Hello, world!"d;
+	btn.margins = Rect(20,20,20,20);
+
+	auto layout = parseML(q{
+			VerticalLayout {
+				margins: 20; padding: 10
+				backgroundColor: "#FFFFE0"
+				TextWidget { text: "Alien Civilisations" }
+				Button {
+					text: "Start new game"
+				}
+				Button { text: "Load save" }
+				Button { text: "Exit" }
+			}
+		});
+	window.mainWidget = layout;
+	window.show();
+	return Platform.instance.enterMessageLoop();
 }
