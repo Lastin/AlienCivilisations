ae5632db15dba2d5d3b4c1db9e108ed51dff3699
Lastin <maksymsmail@gmail.com>
2016-01-17 15:36:14 +0000

    Changed from class to struct, since all data is immutable and publicly accessible

--- AlienCivilisations/src/containers/vector2d.d
+++ AlienCivilisations/src/containers/vector2d.d
@@ -2,25 +2,20 @@
 
 import std.math;
 
-class Vector2d {
-	private immutable float _x;
-	private immutable float _y;
-
-	this(float x, float y){
-		_x = x;
-		_y = y;
-	}
-
-	@property float x() const {
-		return _x;
-	}
-	@property float y() const {
-		return _y;
+struct Vector2d {
+	immutable float x;
+	immutable float y;
+	this(float x_param, float y_param){
+		x = x_param;
+		y = y_param;
 	}
 
 	float getEuclideanDistance(Vector2d vecA){
-		auto xdiff = vecA.x - _x;
-		auto ydiff = vecA.y - _y;
+		auto xdiff = vecA.x - x;
+		auto ydiff = vecA.y - y;
 		return sqrt(xdiff^^2 + ydiff^^2);
+	}
+	Vector2d dup() const {
+		return Vector2d(x, y);
 	}
 }
