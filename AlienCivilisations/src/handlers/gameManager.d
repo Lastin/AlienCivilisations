6b7530bedf86e28dbaac6961cb247d19d8422f0a
Lastin <maksymsmail@gmail.com>
2016-01-17 16:06:49 +0000

    Completes duplication function and moves it to State

--- AlienCivilisations/src/handlers/gameManager.d
+++ AlienCivilisations/src/handlers/gameManager.d
@@ -36,53 +36,6 @@ class GameManager {
 		players ~= new AI(&realState, new KnowledgeTree(_startPoints));
 		return players;
 	}
-
-	State createDuplicate(){
-		State duplicateState;
-		Map mapDup;
-		Player[] playersDup;
-		Ship[] shipsDup;
-		Planet[] planetsDup;
-		//duplicate players
-		foreach(Player origin; _realState.players){
-			string name = origin.name;
-			KnowledgeTree ktDup = origin.knowledgeTree.dup;
-			if(cast(AI)origin){
-				playersDup ~= new AI(&duplicateState, ktDup);
-			}
-			else {
-				playersDup ~= new Player(name, ktDup);
-			}
-		}
-		//duplicate planets
-		foreach(Planet origin; _realState.map.planets){
-			immutable string name = origin.name;
-			immutable float radius = origin.radius;
-			immutable bool bA = origin.breathableAtmosphere;
-			uint food = origin.food;
-			uint militaryUnits = origin.militaryUnits;
-			Player owner = {
-				auto ownerIndex = findOwnerIndex(origin, _realState.players);
-				ownerIndex > -1 ? playersDup[ownerIndex] : null;
-			};
-			Vector2d position = origin.position.dup;
-			uint[8] population = origin.population.dup;
-			planetsDup ~= new Planet(bA, name, position, radius, food, militaryUnits, owner, population);
-		}
-		//duplicate ships
-		foreach(Ship origin; _realState.ships){
-
-		}
-	}
-
-	private size_t findOwnerIndex(Planet planet, Player[] players){
-		foreach(size_t index, Player player; players){
-			if(planet.owner && planet.owner == player){
-				return index;
-			}
-		}
-		return -1;
-	}
 }
 
 /** Container for current, or hypothetical game state, holding references to all essential data **/
@@ -124,5 +77,54 @@ class State {
 		if(++_queuePosition == _players.length){
 			_queuePosition = 0;
 		}
+	}
+
+	State dup() const {
+		State duplicateState;
+		Map mapDup;
+		Player[] playersDup;
+		Planet[] planetsDup;
+		Ship[] shipsDup;
+		//duplicate players
+		foreach(Player origin; players_){
+			string name = origin.name;
+			KnowledgeTree ktDup = origin.knowledgeTree.dup;
+			if(cast(AI)origin){
+				playersDup ~= new AI(&duplicateState, ktDup);
+			}
+			else {
+				playersDup ~= new Player(name, ktDup);
+			}
+		}
+		//duplicate planets
+		foreach(Planet origin; _map.planets){
+			immutable string name = origin.name;
+			immutable float radius = origin.radius;
+			immutable bool bA = origin.breathableAtmosphere;
+			uint food = origin.food;
+			uint militaryUnits = origin.militaryUnits;
+			Player owner = findOwnerIndex(origin, _players, playersDup);
+			Vector2d position = origin.position.dup;
+			uint[8] population = origin.population.dup;
+			uint workForce = origin.workForce;
+			planetsDup ~= new Planet(bA, name, position, radius, food, militaryUnits, owner, population, workForce);
+		}
+		//duplicate map
+		mapDup = new Map(_map.size, planetsDup);
+		//duplicate ships
+		foreach(Ship origin; _realState.ships){
+			Player owner = findOwnerIndex(origin, _players, playersDup);
+			shipsDup ~= new Ship(owner, origin.completed, origin.used);
+		}
+		duplicateState = new State(mapDup, playersDup, shipsDup, _queuePosition);
+	}
+
+	private size_t findOwnerIndex(Owned obj, Player[] origins, Player[] dups){
+		foreach(size_t index, Player player; players){
+			if(obj.owner && obj.owner == player){
+				return dups[index];
+			}
+		}
+		return null;
 	}
 }
